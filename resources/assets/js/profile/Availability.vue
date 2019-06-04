60d39d12d6169349d7a72781aad19dd54d479b11
Brandon <brandon@structur.co>
2019-06-03 21:09:22 -0400

    Create Availability.vue
    - adds availabilty feature to profile.

---
+++ resources/assets/js/profile/Availability.vue
@@ -0,0 +1,28 @@
+<template>
+	<button class="button is-normal is-rounded"  v-bind:class="{ 'is-secondary': isAvailable, 'is-danger': !isAvailable }" style="margin-top: .5rem;" @click="setAvailability"><i class="fa" v-bind:class="{'fa-check': isAvailable, 'fa-times': !isAvailable}" style="margin-right: .5rem;"></i> {{text}}</button>
+</template>
+
+<script>
+	export default {
+		name: 'Availability',
+		props: ['isAvailable'],
+		data() {
+			return {
+				//
+			}
+		},
+		computed: {
+			text() {
+				return this.isAvailable == 0 ? 'Unavailable' : 'Available';
+			},
+		},
+		methods: {
+			setAvailability() {
+				let data = {available: 0};
+				this.isAvailable === 0 ? data.available = 1 : data.available = 0;
+				this.$emit('availability-changed', data.available);
+			},
+		}
+
+	}
+</script>
