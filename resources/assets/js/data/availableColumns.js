3ed01290914fa55becb6e317088740748cdd6367
Brandon <brandon@structur.co>
2020-04-16 15:20:43 -0400

    added desk exerience to avail columns

--- resources/assets/js/data/availableColumns.js
+++ resources/assets/js/data/availableColumns.js
@@ -28,4 +28,5 @@ export default [
 	{ label: 'Earthquake Exp', property: 'earthquake_experience', model: 'work_history',  removable: true},
 	{ label: 'Earthquake Claims', property: 'earthquake_claims', model: 'work_history',  removable: true},
 	{ label: 'Applied ?', property: 'applied', model: 'user',  removable: true },
+	{ label: 'Desk Exp (mo)', property: 'desk_experience', model: 'work_history',  removable: true },
 ]
