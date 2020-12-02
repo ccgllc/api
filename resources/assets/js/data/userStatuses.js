55f94d41f10a3db9215d1d2bb0d9512d6fa69c4f
Brandon <brandon@structur.co>
2020-12-02 14:43:24 -0500

    composer update

---
+++ resources/assets/js/data/userStatuses.js
@@ -0,0 +1,6 @@
+export default [
+	{ name: 'applicant', label: 'Applicant' },
+	{ name: 'active', label: 'Active' },
+	{ name: 'inactive', label: 'Inactive' },
+	{ name: 'no-hire', label: 'No Hire' },
+];
