da4c7608d7a6c261085718c265a4bcf82570ed65
Brandon <brandon@structur.co>
2021-03-19 09:41:27 -0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -146,12 +146,12 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\DomPDF\ServiceProvider::class,
+        // Barryvdh\DomPDF\ServiceProvider::class
         /*
          * Application Service Providers...
          */
