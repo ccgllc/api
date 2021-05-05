2542d2324328258baf18fd6adb71a211410037ea
Brandon <brandon@structur.co>
2021-05-05 07:05:59 -0400

    Update lineItemTypes.js
    re-enabled service fee line item.

--- resources/assets/js/claims/invoice/lineItemTypes.js
+++ resources/assets/js/claims/invoice/lineItemTypes.js
@@ -1,6 +1,6 @@
 export default [
   {name: 'Line Item', type: 'QuantifiableLineItem', quantity: 1, rate: 0, description: 'Description Here', taxable: true},
-	// {name: 'Service Fee Line Item', type: 'ServiceFeeLineItem', description: 'Service Fee', amount: 0, total: 0, taxable: true, fullyReimbursable: false, feeSchedule: {}},
+	{name: 'Service Fee Line Item', type: 'ServiceFeeLineItem', description: 'Service Fee', amount: 0, total: 0, taxable: true, fullyReimbursable: false, feeSchedule: {}},
 	// {name: 'Amount', type: 'AmountLineItem', amount: 0, description: 'Description Here', taxable: true,},
 	// {name: 'Difference In Tiers', type: 'DifferenceInTiersLineItem', amount: 0, newAmount: 0, description: 'Difference In Tiers', taxable: true, fullyReimbursable: false},
 	{name: 'T&E Line Item', type: 'HourlyRateLineItem', quantitiy: 0, rate: 0, description: 'Hourly Rate Line Item', taxable: true, fullyReimbursable: false },
