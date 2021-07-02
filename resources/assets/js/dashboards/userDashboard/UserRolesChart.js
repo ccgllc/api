b060e09e8ec243d1febab1338c14e0dd48d22680
Brandon <brandon@structur.co>
2021-07-02 16:18:46 -0400

    Update UserRolesChart.js

--- resources/assets/js/dashboards/userDashboard/UserRolesChart.js
+++ resources/assets/js/dashboards/userDashboard/UserRolesChart.js
@@ -5,6 +5,22 @@ export default {
         this.chartData.labels = window.dashboardData.userRolesChart.labels;
         this.chartData.datasets[0].data = window.dashboardData.userRolesChart.datasets[0].data;
         this.renderChart(this.chartData, this.options);
+        let chart = this.$data._chart;
+        this.$refs.canvas.onclick = function(evt) {
+            var metric = chart.getElementsAtEvent(evt);
+            if (metric[0]) {
+                var chartData = metric[0]['_chart'].config.data;
+                var idx = metric[0]['_index'];
+
+                var label = chartData.labels[idx];
+                var value = chartData.datasets[0].data[idx];
+
+                var url = "/users/roles/" + label;
+                // console.log(url);
+                // alert(url);
+                window.location = url
+            }
+        }
     },
     data() {
         return {
@@ -12,8 +28,10 @@ export default {
                 labels: [],
                 datasets: [
                     {
-                        backgroundColor: ['#439BD1'],
-                        data: []
+                        backgroundColor: ['#30a987','#343b4c', '#439BD1', '#dedddd', '#ffe08a'],
+                        data: [],
+                        categoryPercentage: 0.75,
+                        barPercentage: 0.75
                     }
                 ]
             },
@@ -28,16 +46,14 @@ export default {
                       beginAtZero: true
                     },
                     gridLines: {
-                      display: false
+                      display: true
                     }
                   }],
-                  xAxes: [ {
+                  xAxes: [{
                     gridLines: {
                       display: false
                     },
-                    categoryPercentage: 0.5,
-                    barPercentage: 0.2
-                  }]
+                  }],
                 }
             } 
         }
