8645d6c8e2c4d3d2ceaab6e8ee2e99eacd247ec0
Brandon <brandon@structur.co>
2021-07-02 16:19:10 -0400

    Update dispatch.js

--- resources/assets/js/dispatch/dispatch.js
+++ resources/assets/js/dispatch/dispatch.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import dashboard from './Dashboard.vue';
+import search from '../components/Search.vue';
 let vue = new Vue({
 	el: '#dashboard',
 	components: {
-		dashboard
+		dashboard, search
 	}
 });
